7b8d9c6d1e045fb9c1c9eb8eff25c8134c059f1f
MCargnin <mvcargnin@gmail.com>
2022-10-20 23:32:33 -0300

    Commit exercício 3

---
+++ Exercise 3 - Barking Dog/BarkingDog.java
@@ -0,0 +1,8 @@
+public class BarkingDog {
+    public static boolean shouldWakeUp (boolean barking, int hourOfDay){
+        if((hourOfDay >= 0 && hourOfDay < 8) || (hourOfDay > 22 && hourOfDay <= 23)){
+            return barking;
+        }
+        return false;
+    }
+}
